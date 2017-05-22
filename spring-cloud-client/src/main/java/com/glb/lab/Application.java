7fbb9e256a4b6b912c0e44e0386c39123f9c878a
William <william.diaz@yahoo.es>
2017-05-22 07:35:12 -0500

    [William] Initial Commit

---
+++ spring-cloud-client/src/main/java/com/glb/lab/Application.java
@@ -0,0 +1,16 @@
+package com.glb.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by william.diaz on 5/8/2017.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
