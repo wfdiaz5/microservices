272580a968d79bd140d655accceee624944a6e7a
william.diaz <wfdiaz5@gmail.com>
2017-12-26 17:33:12 -0500

    [William] Updated some ports and attributes

--- spring-discovery-server/src/main/java/com/glb/lab/EurekaServerApp.java
+++ spring-discovery-server/src/main/java/com/glb/lab/EurekaServerApp.java
@@ -3,15 +3,13 @@ package com.glb.lab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class EurekaServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaServerApp.class, args);
     }
 
 
