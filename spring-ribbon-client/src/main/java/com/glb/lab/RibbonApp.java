98a9d315afd958f7228212eb6152e9c6e8848fbc
william.diaz <wfdiaz5@gmail.com>
2018-01-31 14:27:11 -0500

    [William] Rename class

---
+++ spring-ribbon-client/src/main/java/com/glb/lab/RibbonApp.java
@@ -0,0 +1,26 @@
+package com.glb.lab;
+
+import com.github.javafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients //Runtime: Spring will seek interfaces and implement them.
+public class RibbonApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApp.class, args);
+    }
+}
